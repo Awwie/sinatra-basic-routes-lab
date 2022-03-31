701c3260f87fd808ff2da43655984103dbc2df04
Awwie <acecace@live.com>
2022-03-31 07:02:50 -0500

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,25 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    # get '/' do
+    #     "Hello World"
+    #   end
+
+    get '/name' do
+        "My name is Anthony."
+    end 
+
+    get '/hometown' do
+        "My hometown is Crete, IL."
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is Lowrider."
+    end 
+
 end
+
+# "My name is __"
+# "My hometown is __"
+# "My favorite song is __"
